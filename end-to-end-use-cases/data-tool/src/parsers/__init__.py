2a128ccbbd14c3d716db5adf6058868b9acfd5d9
Sanyam Bhutani <sanyam.bhutani05@gmail.com>
2025-03-02 18:20:41 -0800

    Create __init__.py

---
+++ end-to-end-use-cases/data-tool/src/parsers/__init__.py
@@ -0,0 +1,6 @@
+from .pdf_parser import PDFParser
+from .html_parser import HTMLParser
+from .youtube_parser import YouTubeParser
+from .docx_parser import DOCXParser
+from .txt_parser import TXTParser
+from .ppt_parser import PPTParser
